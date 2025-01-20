d81616cc91328a364b67c05f24db072a4b3a7d98
David <dhyoon@olimplanet.com>
2025-01-20 10:47:50 +0900

    불필요 주석 제거

--- apps/web/three/src/pages/GameLand.tsx
+++ apps/web/three/src/pages/GameLand.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-import { Fisheye, CameraControls, PerspectiveCamera, Environment, Billboard, Text, KeyboardControls } from '@react-three/drei'
+import { Fisheye, Environment, Billboard, Text } from '@react-three/drei'
 import { Level, Sudo, Camera, Cactus, Box } from '../components/template/GameLandScene'
 import { Color } from 'three'
 import WasdControls from '../components/molecule/WASDControls'
@@ -9,7 +9,6 @@ export default function GameLand() {
     <Canvas style={{width: '100vw', height: '100vh'}} flat>
       <Fisheye zoom={0}>
         <WasdControls />
-        {/* <CameraControls minPolarAngle={0} maxPolarAngle={Math.PI / 1.6} /> */}
         <ambientLight intensity={Math.PI / 2} />
         <group scale={20} position={[5, -11, -5]}>
           <Level />
@@ -19,7 +18,6 @@ export default function GameLand() {
           <Box position={[-0.8, 1.4, 0.4]} scale={0.15} />
         </group>
         <Environment preset="dawn" background blur={1} />
-        {/* <PerspectiveCamera makeDefault position={[0, 0, 18.5]} /> */}
         <Billboard
           position={[3, 3, 3]}
           follow={true}
