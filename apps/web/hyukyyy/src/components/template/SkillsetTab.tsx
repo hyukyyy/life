efbd8e72b01cb14c8d691401b0c5e66e1ed56b03
David <dhyoon@olimplanet.com>
2025-02-14 19:05:21 +0900

    스킬셋 탭 import

--- apps/web/hyukyyy/src/components/template/SkillsetTab.tsx
+++ apps/web/hyukyyy/src/components/template/SkillsetTab.tsx
@@ -2,20 +2,66 @@ export default function SkillsetTab() {
     return <section className={`flex items-center justify-center`}>
         <div className={`rounded-lg shadow-sm bg-black px-10 py-5`}>
             <ul>
-                <li className={`flex items-center justify-center`}>
+                <li className={`flex items-center justify-center gap-4 my-4`}>
                     <span>language</span>
-                    <div>
-
+                    <div className={`flex flex-row gap-2`}>
+                        <img
+                            src={`https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E`}
+                            alt={`javascript`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white`}
+                            alt={`typescript`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white`}
+                            alt={`typescript`}
+                        />
                     </div>
                 </li>
-                <li className={`flex items-center justify-center`}>
+                <li className={`flex items-center justify-center gap-4 my-4`}>
+                    <span>library</span>
+                    <div className={`flex flex-row gap-2`}>
+                        <img
+                            src={`https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB`}
+                            alt={`react`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB`}
+                            alt={`react-native`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/-React%20Query-FF4154?style=for-the-badge&logo=react%20query&logoColor=white`}
+                            alt={`react-query`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white`}
+                            alt={`redux`}
+                        />
+
+                        <img
+                            src={`https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white`}
+                            alt={`tailwindcss`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white`}
+                            alt={`styled-component`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/-React%20Query-FF4154?style=for-the-badge&logo=react%20query&logoColor=white`}
+                            alt={`react-query`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/-React%20Query-FF4154?style=for-the-badge&logo=react%20query&logoColor=white`}
+                            alt={`react-query`}
+                        />
+                    </div>
+                </li>
+                <li className={`flex items-center justify-center gap-4 my-4`}>
                     <span>language</span>
                 </li>
-                <li className={`flex items-center justify-center`}>
-                    <span>language</span>
-                </li>
-                <li className={`flex items-center justify-center`}>
-                    <span>language</span>
+                <li className={`flex items-center justify-center gap-4 my-4`}>
+                <span>language</span>
                 </li>
             </ul>
         </div>
