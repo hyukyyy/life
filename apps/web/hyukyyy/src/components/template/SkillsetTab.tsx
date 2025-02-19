e5d409174b1e0ec22d88e74176bf11ee4f00ed49
David <dhyoon@olimplanet.com>
2025-02-19 18:17:03 +0900

    스킬셋 탭 추가

--- apps/web/hyukyyy/src/components/template/SkillsetTab.tsx
+++ apps/web/hyukyyy/src/components/template/SkillsetTab.tsx
@@ -1,10 +1,12 @@
 export default function SkillsetTab() {
-    return <section className={`flex items-center justify-center`}>
-        <div className={`rounded-lg shadow-sm bg-black px-10 py-5`}>
+    return <section className={`flex items-center justify-center mx-2`}>
+        <div className={`rounded-lg shadow-sm px-10 py-10`}>
             <ul>
-                <li className={`flex items-center justify-center gap-4 my-4`}>
-                    <span>language</span>
-                    <div className={`flex flex-row gap-2`}>
+                <li className={`flex items-center gap-4 my-4`}>
+                    <span className={`font-bold underline underline-offset-4 text-lg w-[80px]`}>
+                        language
+                    </span>
+                    <div className={`flex flex-row gap-2 overflow-x-auto`}>
                         <img
                             src={`https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E`}
                             alt={`javascript`}
@@ -19,9 +21,11 @@ export default function SkillsetTab() {
                         />
                     </div>
                 </li>
-                <li className={`flex items-center justify-center gap-4 my-4`}>
-                    <span>library</span>
-                    <div className={`flex flex-row gap-2`}>
+                <li className={`flex items-center gap-4 my-4`}>
+                    <span className={`font-bold underline underline-offset-4 text-lg w-[80px]`}>
+                        library
+                    </span>
+                    <div className={`flex flex-row gap-2 overflow-x-auto`}>
                         <img
                             src={`https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB`}
                             alt={`react`}
@@ -52,16 +56,57 @@ export default function SkillsetTab() {
                             alt={`react-query`}
                         />
                         <img
-                            src={`https://img.shields.io/badge/-React%20Query-FF4154?style=for-the-badge&logo=react%20query&logoColor=white`}
-                            alt={`react-query`}
+                            src={`https://img.shields.io/badge/threejs-black?style=for-the-badge&logo=three.js&logoColor=white`}
+                            alt={`three.js`}
                         />
                     </div>
                 </li>
-                <li className={`flex items-center justify-center gap-4 my-4`}>
-                    <span>language</span>
+                <li className={`flex items-center gap-4 my-4`}>
+                    <span className={`font-bold underline underline-offset-4 text-lg w-[80px]`}>
+                        framework
+                    </span>
+                    <div className={`flex flex-row gap-2 overflow-x-auto`}>
+                        <img
+                            src={`https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white`}
+                            alt={`nextjs`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/nestjs-%23E0234E.svg?style=for-the-badge&logo=nestjs&logoColor=white`}
+                            alt={`nestjs`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white`}
+                            alt={`spring`}
+                        />
+                    </div>
                 </li>
-                <li className={`flex items-center justify-center gap-4 my-4`}>
-                <span>language</span>
+                <li className={`flex items-center gap-4 my-4`}>
+                    <span className={`font-bold underline underline-offset-4 text-lg w-[80px]`}>
+                        tool
+                    </span>
+                    <div className={`flex flex-row gap-2 overflow-x-auto`}>
+                        <img
+                            src={`https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white`}
+                            alt={`vite`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/pnpm-%234a4a4a.svg?style=for-the-badge&logo=pnpm&logoColor=f69220`}
+                            alt={`pnpm`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white`}
+                            alt={`github`}
+                        />
+                        <img
+                            src={`https://img.shields.io/badge/AWS-%23FF9900.svg?style=for-the-badge&logo=amazon-aws&logoColor=white`}
+                            alt={`aws`}
+                        />
+
+                        <img
+                            src={`https://img.shields.io/badge/webstorm-143?style=for-the-badge&logo=webstorm&logoColor=white&color=black`}
+                            alt={`webstorm`}
+                        />
+                    </div>
                 </li>
             </ul>
         </div>
